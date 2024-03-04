ae0dfc2b00964ea20d628b264a66b5179a48526f
Nicollas Gomes <103591616+Nicollas-Gomes@users.noreply.github.com>
2024-03-04 19:51:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionaisCsharp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis
+            int urg;
+            
+
+            //entrada do usuario
+            Console.WriteLine("Digite seu nível de urgência");
+            urg = int.Parse(Console.ReadLine());
+
+            if(urg <= 3)
+            {
+                Console.WriteLine("Seu nível de urgência é BAIXO");
+            }
+            else if(urg > 3 & urg <= 6)
+            {
+                Console.WriteLine("Seu nível de urgência é MEDIO");
+            }
+            else if (urg > 6 & urg <= 9)
+            {
+                Console.WriteLine("Seu nível de urgência é ALTO");
+            }
+            else
+            {
+                Console.WriteLine("Seu nível de urgência é GRAVE");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
